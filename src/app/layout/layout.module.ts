bd9bad40ff81b9ab3b90d1dbc602af733131f726
godoi <andersongodoipoa@gmail.com>
2020-02-22 17:58:34 -0300

    Implementing list box component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { CharactersComponent } from '../home/characters/characters.component';
+import { ListBoxComponent } from './list-box/list-box.component';
+import { HomeRoutingModule } from '../home/home-routing.module';
 
 @NgModule({
-  declarations: [HeaderComponent, NotFoundComponent],
-  imports: [CommonModule],
-  exports: [HeaderComponent, NotFoundComponent],
-  entryComponents: [CharactersComponent]
+  declarations: [HeaderComponent, NotFoundComponent, ListBoxComponent],
+  imports: [CommonModule, HomeRoutingModule],
+  exports: [
+    HeaderComponent,
+    NotFoundComponent,
+    ListBoxComponent,
+    HomeRoutingModule
+  ]
 })
 export class LayoutModule {}
