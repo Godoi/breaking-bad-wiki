77f4b659e7b17e382d4236ca7427754f2e57c1a2
godoi <andersongodoipoa@gmail.com>
2020-02-28 20:28:47 -0300

    implementing e2e test

--- cypress/integration/application/home.spec.js
+++ cypress/integration/application/home.spec.js
@@ -1,28 +1,28 @@
 context('Application Breaking Bad', () => {
-    describe('Accessing The Home Page', function () {
-        it('successfully loads', function () {
-            cy.visit('http://localhost:4200/');
-            cy.location().should((loc) => {
-                expect(loc.href).to.eq('http://localhost:4200/home')
-            });
-        });
+  describe('Accessing The Home Page', function() {
+    it('successfully loads', function() {
+      cy.visit('http://localhost:4200/');
+      cy.location().should(loc => {
+        expect(loc.href).to.eq('http://localhost:4200/home');
+      });
     });
-    describe('Accessing the Characters page through the main page button', function () {
-        it('successfully loads Characters page', function () {
-            cy.visit('http://localhost:4200/');
-            cy.get('[data-cy=btn-all-characters]').click();
-            cy.location().should((loc) => {
-                expect(loc.href).to.eq('http://localhost:4200/home/characters')
-            });
-        });
+  });
+  describe('Accessing the Characters page through the main page button', function() {
+    it('successfully loads Characters page', function() {
+      cy.visit('http://localhost:4200/');
+      cy.get('[data-cy=btn-all-characters]').click();
+      cy.location().should(loc => {
+        expect(loc.href).to.eq('http://localhost:4200/home/characters');
+      });
     });
-    describe('Accessing the Characters page through the main page menu', function () {
-        it('successfully loads Characters page', function () {
-            cy.visit('http://localhost:4200/');
-            cy.get('[data-cy=menu-characters]').click();
-            cy.location().should((loc) => {
-                expect(loc.href).to.eq('http://localhost:4200/home/characters')
-            });
-        });
+  });
+  describe('Accessing the Characters page through the main page menu', function() {
+    it('successfully loads Characters page', function() {
+      cy.visit('http://localhost:4200/');
+      cy.get('[data-cy=menu-characters]').click();
+      cy.location().should(loc => {
+        expect(loc.href).to.eq('http://localhost:4200/home/characters');
+      });
     });
+  });
 });
