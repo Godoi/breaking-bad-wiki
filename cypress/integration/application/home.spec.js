f8a7fd1e63ae2c12cdbefa3ab04bf6174fbdf716
godoi <andersongodoipoa@gmail.com>
2020-02-28 15:28:54 -0300

    implementing e2e test

---
+++ cypress/integration/application/home.spec.js
@@ -0,0 +1,28 @@
+context('Application Breaking Bad', () => {
+    describe('Accessing The Home Page', function () {
+        it('successfully loads', function () {
+            cy.visit('http://localhost:4200/');
+            cy.location().should((loc) => {
+                expect(loc.href).to.eq('http://localhost:4200/home')
+            });
+        });
+    });
+    describe('Accessing the Characters page through the main page button', function () {
+        it('successfully loads Characters page', function () {
+            cy.visit('http://localhost:4200/');
+            cy.get('.btn-link').click();
+            cy.location().should((loc) => {
+                expect(loc.href).to.eq('http://localhost:4200/home/characters')
+            });
+        });
+    });
+    describe('Accessing the Characters page through the main page menu', function () {
+        it('successfully loads Characters page', function () {
+            cy.visit('http://localhost:4200/');
+            cy.get('.nav-item.active a').eq(0).click();
+            cy.location().should((loc) => {
+                expect(loc.href).to.eq('http://localhost:4200/home/characters')
+            });
+        });
+    });
+});
