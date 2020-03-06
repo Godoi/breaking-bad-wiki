31b43375d032f5b7dda8321a4c0ad803761df7f5
anderson.bernardes <anderson.bernardes@thomsonreuters.com>
2020-03-06 17:42:04 -0300

    Putting the css in the default

--- src/app/layout/list-box/list-box.component.ts
+++ src/app/layout/list-box/list-box.component.ts
@@ -4,7 +4,6 @@ import { ICharacters } from 'src/app/shared/model/characters';
 @Component({
   selector: 'app-list-box',
   templateUrl: './list-box.component.html',
-  styleUrls: ['./list-box.component.scss']
 })
 export class ListBoxComponent implements OnInit {
   @Input()
